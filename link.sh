61e34161282dc2addcb81915d9344f0565a20d21
Ruben Jongejan <ruben.jongejan@gmail.com>
2018-03-25 19:27:24 +0200

    added dunstrc

--- link.sh
+++ link.sh
@@ -6,14 +6,11 @@ rm ~/.config/xfce4/terminal/terminalrc
 ln -s ~/.dotfiles/terminalrc ~/.config/xfce4/terminal/terminalrc
 rm ~/.bashrc
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
-rm ~/.config/polybar/config
-ln -s ~/.dotfiles/polybar ~/.config/polybar/config
 rm ~/.config/compton.conf
 ln -s ~/.dotfiles/compton.conf ~/.config/compton.conf
 rm ~/.i3blocks.conf
 ln -s ~/.dotfiles/.i3blocks.conf ~/.i3blocks.conf
-rm ~/.config/termite/config
-mkdir -p ~/.config/termite
-ln -s ~/.dotfiles/termite.config ~/.config/termite/config
 rm ~/.tmux.conf
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+rm ~/.config/dunstrc
+ln -s ~/.dotfiles/dunstrc ~/.config/dunstrc
